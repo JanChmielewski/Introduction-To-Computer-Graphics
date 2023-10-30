fbd41b145df766c0cfc379fddc6994e721c82795
JanChmielewski <52100333+JanChmielewski@users.noreply.github.com>
2023-10-30 22:26:59 +0100

    fixed sidealks, added grass inside of them and lane dividers

--- CityMap/main.js
+++ CityMap/main.js
@@ -28,36 +28,74 @@ const cityBase = new THREE.Mesh(cityBaseGeometry, cityBaseMaterial);
 cityBase.position.set(0, 0, 0)
 cityBlock.add(cityBase);
 
-const sidewalkGeometry = new THREE.BoxGeometry(0.05, 0.02, 1);
+const sidewalkGeometry = new THREE.BoxGeometry(0.05, 0.01, 0.4);
 const sideWalkMaterial = new THREE.MeshBasicMaterial({color: 0x737576})
 const verticalSidewalk = new THREE.Mesh(sidewalkGeometry, sideWalkMaterial);
 const horizontalSidewalk = new THREE.Mesh(sidewalkGeometry, sideWalkMaterial);
-
-const buildingSurrounding = new THREE.Group();
-const buildingSurroundingGeometry = new THREE.BoxGeometry(0.05, 0.02, 0.25);
-const buildingSurroundingMaterial = new THREE.MeshBasicMaterial({color: 0x737576})
-const horizontalSurr = new THREE.Mesh(buildingSurroundingGeometry, buildingSurroundingMaterial);
-const verticalSurr = new THREE.Mesh(buildingSurroundingGeometry, buildingSurroundingMaterial);
-
-buildingSurrounding.add();
-cityBlock.add(buildingSurrounding);
-
 
 const leftVerticalSidewalk = verticalSidewalk.clone();
 const rightVerticalSidewalk = verticalSidewalk.clone();
 const frontHorizontalSidewalk = horizontalSidewalk.clone();
 const backHorizontalSidewalk = horizontalSidewalk.clone();
 
+const buildingSurr = new THREE.Group();
 
-
-leftVerticalSidewalk.position.set(-0.475, 0.01, 0);
-rightVerticalSidewalk.position.set(0.475, 0.01, 0);
-frontHorizontalSidewalk.position.set(0, 0.01, 0.475);
+leftVerticalSidewalk.position.set(-0.475, 0.01, -0.3);
+rightVerticalSidewalk.position.set(-0.125, 0.01, -0.3);
+frontHorizontalSidewalk.position.set(-0.3, 0.01, -0.125);
 frontHorizontalSidewalk.rotateY(Math.PI/2);
-backHorizontalSidewalk.position.set(0, 0.01, -0.475);
+backHorizontalSidewalk.position.set(-0.3, 0.01, -0.475);
 backHorizontalSidewalk.rotateY(Math.PI/2);
+buildingSurr.add(leftVerticalSidewalk, rightVerticalSidewalk, frontHorizontalSidewalk, backHorizontalSidewalk);
 
-cityBlock.add(leftVerticalSidewalk, rightVerticalSidewalk, frontHorizontalSidewalk, backHorizontalSidewalk);
+const frontLeftBuildingSurr = buildingSurr.clone();
+const frontRightBuildingSurr = buildingSurr.clone();
+const backRightBuildingSurr = buildingSurr.clone();
+
+frontLeftBuildingSurr.position.set(0, 0, 0.6);
+frontRightBuildingSurr.position.set(0.6, 0, 0.6)
+backRightBuildingSurr.position.set(0.6, 0, 0)
+
+cityBlock.add(buildingSurr, frontLeftBuildingSurr, frontRightBuildingSurr, backRightBuildingSurr);
+
+const grassGeometry = new THREE.BoxGeometry(0.3, 0.01, 0.3);
+const grassMaterial = new THREE.MeshBasicMaterial({color: 0x23E044})
+const grass = new THREE.Mesh(grassGeometry, grassMaterial);
+
+const frontLeftGrass = grass.clone();
+const frontRightGrass = grass.clone();
+const backLeftGrass = grass.clone();
+const backRightGrass = grass.clone();
+
+frontLeftGrass.position.set(-0.3, 0.01,0.3);
+frontRightGrass.position.set(0.3, 0.01,0.3);
+backLeftGrass.position.set(-0.3, 0.01,-0.3);
+backRightGrass.position.set(0.3, 0.01,-0.3);
+
+cityBlock.add(frontLeftGrass, frontRightGrass, backLeftGrass, backRightGrass);
+
+const laneDividersGeometry = new THREE.BoxGeometry(0.005, 0.0001, 0.015);
+const laneDividersMaterial = new THREE.MeshBasicMaterial({colorL: 0xFFFFFF});
+const laneDivider = new THREE.Mesh(laneDividersGeometry, laneDividersMaterial);
+
+const laneDividers = new THREE.Group();
+
+// to print lane dividers
+for (let position = 0.485; position >= -0.5; position -= 0.025) {
+    if(position > 0.11 || position < -0.11) {
+        const verticalDivider = laneDivider.clone();
+        verticalDivider.position.set(0, 0.01, position);
+
+        const horizontalDivider = laneDivider.clone();
+        horizontalDivider.rotateY(Math.PI / 2);
+        horizontalDivider.position.set(position, 0.01, 0);
+
+        laneDividers.add(verticalDivider, horizontalDivider);
+    }
+}
+
+
+cityBlock.add(laneDividers);
 
 function animate() {
     requestAnimationFrame(animate);
